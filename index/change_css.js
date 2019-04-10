92df721615dbbb6c9f02a5dc441b81f0dbe01fbe
gnepnaiL-oahZ <37571698+gnepnaiL-oahZ@users.noreply.github.com>
2019-04-10 18:05:45 +0800

    Update change_css.js

--- index/change_css.js
+++ index/change_css.js
@@ -1,9 +1,12 @@
 {
   let btns = document.getElementsByTagName("button");
-  let css = document.getElementById("different_css");
+  let css = document.createElement("link");
+  css.rel="stylesheet";
+  css.href="";
   for (let i = 0; i < btns.length; i++) {
     btns[i].onclick = function () {
       css.setAttribute("href", btns[i].innerHTML);
     };
   }
+  document.getElementsByTagName("head")[0].appendChild(css);
 }
