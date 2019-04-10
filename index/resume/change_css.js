b7d19b5c97373a7eac4d86443c492c8fbea180b8
gnepnaiL-oahZ <37571698+gnepnaiL-oahZ@users.noreply.github.com>
2019-04-10 17:17:31 +0800

    condense codes of lesson of resume

---
+++ index/resume/change_css.js
@@ -0,0 +1,7 @@
+let btns = document.getElementsByTagName("button");
+let css = document.getElementById("different_css");
+for (let i = 0; i < btns.length; i++) {
+  btns[i].onclick = function () {
+    css.setAttribute("href",btns[i].id + ".css");
+  };
+}
